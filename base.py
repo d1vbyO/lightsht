baaa49e80b9fdb64d3751236e5b40645deeda3ce
firstbirth <60266678+pericle223@users.noreply.github.com>
2022-04-25 02:37:49 +0600

    Create base.py

---
+++ base.py
@@ -0,0 +1,29 @@
+import telebot
+from telebot import types
+import random
+
+API_TOKEN = '5338405542:AAE6ofae1WEH6eF7Jhxf83HNINyEOJvBXlM'  # - бот тестовый
+bot = telebot.TeleBot(API_TOKEN)
+
+symbols = '1234567890abcdefghijklnopqrstuvwxyz'
+id_length = 6
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    nextbnt = types.KeyboardButton('===>ON<===')
+    markup.add(nextbnt)
+    bot.send_message(message.chat.id,'Бот для просмотра картнок с lightshot', reply_markup=markup, parse_mode="Markdown")
+
+@bot.message_handler(content_types=['text'])
+def send_pic(message):
+    if message.text == '===>ON<===':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        nextbnt = types.KeyboardButton('===>ON<===')
+        markup.add(nextbnt)
+        image_id = ''
+        for item in range(id_length):
+            image_id += random.choice(symbols)
+        bot.send_message(message.chat.id, 'prnt.sc/' + image_id, reply_markup=markup, parse_mode="Markdown")
+
+bot.polling(True)
